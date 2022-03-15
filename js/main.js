d431e82c0e538a5c2fbc7556e2cac16fd97d7db9
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-03-15 18:09:20 +0300

    Готовая главная страница
    без адаптивного меню

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+	$('.slider_illustration_inner').slick({
+		lazyLoad: 'ondemand',
+		prevArrow: '<button type="button" class="slick-prev-ill slick-btn-ill"><span class="icon-caret-left icon-ill"></span></button>',
+		nextArrow: '<button type="button" class="slick-next-ill slick-btn-ill"><span class="icon-caret-right icon-ill"></span></button>'
+	});
+
+
+});
