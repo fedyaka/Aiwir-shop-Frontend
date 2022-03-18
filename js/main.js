aef48a34ebf97d5c3357c6e5cb958ffebf77ace5
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-03-18 15:22:34 +0300

    Мобильное меню

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(function(){
-
-
 	$('.slider_illustration_inner').slick({
 		lazyLoad: 'ondemand',
 		prevArrow: '<button type="button" class="slick-prev-ill slick-btn-ill"><span class="icon-caret-left icon-ill"></span></button>',
 		nextArrow: '<button type="button" class="slick-next-ill slick-btn-ill"><span class="icon-caret-right icon-ill"></span></button>'
 	});
 
-	
 
-
+	$('.pushmenu').on('click', function() {
+		$('.pushmenu').toggleClass('pushmenu_active');
+		$('.menu-list').toggleClass('menu_active');
+		$('.content').toggleClass('content_active');
+	});
 });
+
