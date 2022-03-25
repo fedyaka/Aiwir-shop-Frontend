688dc1f91c04164b330e576c61e5f6875bf4e173
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-03-25 20:27:58 +0300

    адаптивная страница заказов

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,12 @@ $(function(){
 		prevArrow: '<button type="button" class="slick-prev-ill slick-btn-ill"><span class="icon-caret-left icon-ill"></span></button>',
 		nextArrow: '<button type="button" class="slick-next-ill slick-btn-ill"><span class="icon-caret-right icon-ill"></span></button>'
 	});
+	
+	$('.slider-illustration-examples__inner').slick({
+		lazyLoad: 'ondemand',
+		prevArrow: '<button type="button" class="slick-prev-ill slick-btn-ill"><span class="icon-caret-left icon-ill"></span></button>',
+		nextArrow: '<button type="button" class="slick-next-ill slick-btn-ill"><span class="icon-caret-right icon-ill"></span></button>'
+	});
 
 
 	$('.menu__pushmenu').on('click', function() {
